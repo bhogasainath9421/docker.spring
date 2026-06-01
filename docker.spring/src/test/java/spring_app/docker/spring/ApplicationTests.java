e70be5bce3f42b79a4e0419d00c496f81eedd8cc
bhogasainath9421 <bhogasainath9421@gmail.com>
2026-06-01 13:10:58 +0530

    Initial commit

---
+++ docker.spring/src/test/java/spring_app/docker/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_app.docker.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
