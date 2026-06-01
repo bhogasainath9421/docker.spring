f1ce85e7ee01f6c345bcc85ce935fd4d9ea86183
bhogasainath9421 <153518611+bhogasainath9421@users.noreply.github.com>
2026-06-01 15:17:23 +0530

    Refactor Application class by removing servlet support

--- docker.spring/src/main/java/spring_app/docker/spring/Application.java
+++ docker.spring/src/main/java/spring_app/docker/spring/Application.java
@@ -2,19 +2,12 @@ package spring_app.docker.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
